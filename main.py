71187f32ea4cedc32d5d84c68d63da97d5df81c3
Tero Piirainen <tero.piirainen@gmail.com>
2025-10-30 23:38:23 +0200

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 # hello_world.py
 
 print("Hello, World!")
+print("This is a simple Python script.")
+print("Goodbye, World!")
+# main.py
+from hello_world import *
+# This file imports everything from hello_world.py
+# and does not contain any additional code.
+
+# It serves to demonstrate the import functionality.
+
+# When you run main.py, it will execute the code in hello_world.py
+# and print the messages to the console.
+
+# To run the script, use the command: python main.py
+
+# Ensure that both hello_world.py and main.py are in the same directory.
+
+# The output will be:
+# Hello, World!
+# This is a simple Python script.
+# Goodbye, World!
